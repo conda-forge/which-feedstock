2f8fdb26a0dafdf115e9e3e1b5b262a18fd6f0e6
Matthew Feickert <matthew.feickert@cern.ch>
2026-04-17 18:41:05 -0600

    MNT: Add necessary components for safe cross-compilation

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,18 +2,24 @@
 
 set -euxo pipefail
 
+# Get an updated config.sub and config.guess
+cp $BUILD_PREFIX/share/gnuconfig/config.* .
+
 ./configure --prefix=${PREFIX}        \
             --libdir=${PREFIX}/lib    \
             --build=${BUILD}          \
             --host=${HOST}
 
 make -j${CPU_COUNT}
-if [[ "$build_platform" == "$target_platform" ]]; then
-  make check
+
+# Skip 'make check' when cross-compiling
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR:-}" != "" ]]; then
+  make check -j${CPU_COUNT}
 fi
+
 make install
 
-for CHANGE in "activate" 
+for CHANGE in "activate"
 do
     mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
     cp "${RECIPE_DIR}/${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
